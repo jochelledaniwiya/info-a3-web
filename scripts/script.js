5a554520dbac6c800930179d1bde14bf0668082e
Jochelle <Jochelledaniwiya@gmail.com>
2026-05-15 15:28:26 +1000

    basic js for products

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,48 @@
-products = {
-    "product1": {
-        title:"",
-        imgsrc1:"",
+const products = {
+    sencha: {
+        title: "Jasmine Sencha Green Tea",
+        price: "AUD $20.00",
+        imgsrc1: "images/jpsencha.png",
     },
-    "product2": {
+    bancha: {
+        title: "Japanese Bancha Green Tea",
+        price: "AUD $20.00",
+        imgsrc1: "images/jpbancha.jpg",
+    },
+    lime: {
+        title: "Japanese Lime Green Tea",
+        price: "AUD $20.00",
+        imgsrc1: "images/jplime.jpg",
+    },
+    jasminegreen: {
+        title: "Organic Jasmine Green Tea",
+        price: "AUD $15.00",
+        imgsrc1: "images/jasminegreen.jpg",
+    },
+    darjeeling: {
+        title: "Darjeeling",
+        price: "AUD $20.00",
+        imgsrc1: "images/darjeeling.jpg",
+    },
+    balance: {
+        title: "Balance",
+        price: "AUD $15.00",
+        imgsrc1: "images/balance.jpg",
+    },
+    dreams: {
+        title: "Sweet Dreams",
+        price: "AUD $15.00",
+        imgsrc1: "images/sweetdreams.jpg",
+    },
+};
 
-    }
-}
+const params = URLSearchParams(window.location.search);
+const currProductId = params.get("product");
+const productData = products[currProductId];
 
-const params = URLSearchParams()
-const currProductId = params.get("product")
-
-function initPage(product){
+function initPage() {
     const productData = products[product]
 
-    document.getElementById("").src = productData.imgsrc1
+    document.getElementById("product-title").textContent = productData.title;
+    document.getElementById("main-product-img").src = productData.imgsrc1;
 }
